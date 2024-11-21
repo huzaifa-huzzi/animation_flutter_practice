8caae509120ee168d134ae09225ba7b023e346e4
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-11-21 15:47:37 +0500

    1.Practicing the animations

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    animation = Tween<double>(begin: -1, end: 0).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.fastOutSlowIn),
+    );
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Retrieve screen width
+    final double width = MediaQuery.of(context).size.width;
+
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Scaffold(
+          body: Transform(
+            transform: Matrix4.translationValues(animation.value * width, 0.0, 0.0),
+            child: Center(
+              child: Container(
+                child: const Text('Login'),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
