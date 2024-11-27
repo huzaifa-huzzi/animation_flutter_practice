75c8f314344858f79ae93b34f6a82db46818fcb6
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-11-27 15:20:18 +0500

    1.Practicing the animations

--- lib/ParentingAnimations.dart
+++ lib/ParentingAnimations.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class ParentingAnimations extends StatefulWidget {
   const ParentingAnimations({super.key});
 
@@ -9,32 +7,94 @@ class ParentingAnimations extends StatefulWidget {
   State<ParentingAnimations> createState() => _ParentingAnimationsState();
 }
 
-class _ParentingAnimationsState extends State<ParentingAnimations> with SingleTickerProviderStateMixin {
-
-  late Animation animation,childAnimation;
-  late AnimationController animationController ;
+class _ParentingAnimationsState extends State<ParentingAnimations>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation, childAnimation;
+  late AnimationController animationController;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    animationController = AnimationController(vsync: this,duration: const  Duration(seconds: 10));
 
-    animation = Tween(begin: -0.1,end: 0).animate(CurvedAnimation(parent: animationController, curve: Curves.easeIn));
+    // Initialize the animation controller
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
 
-    childAnimation = Tween(begin: 20,end: 125).animate(CurvedAnimation(parent: animationController, curve: Curves.easeIn));
+    // Define animations using CurvedAnimation for better transitions
+    animation = Tween<double>(begin: -0.1, end: 0.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeIn,
+      ),
+    );
 
+    childAnimation = Tween<double>(begin: 20.0, end: 125.0).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    // Start the animation
+    animationController.forward();
   }
+
 
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return AnimatedBuilder(
-        animation: animationController,
-        builder: (context,index){
-          return
 
-        });
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (context, child) {
+        return Scaffold(
+          body: Align(
+            alignment: Alignment.center,
+            child: Container(
+              child: Transform(
+                transform: Matrix4.translationValues(
+                  animation.value * width, // animation.value is now a double
+                  0,
+                  0,
+                ),
+                child: AnimatedBuilder(
+                  animation: childAnimation,
+                  builder: (context, child) {
+                    return Center(
+                      child: Container(
+                        padding: const EdgeInsets.all(15),
+                        height: childAnimation.value * 2,
+                        width: childAnimation.value * 2,
+                        child: ListView(
+                          children: const [
+                            TextField(
+                              decoration: InputDecoration(hintText: 'Username'),
+                            ),
+                            SizedBox(height: 15),
+                            TextField(
+                              decoration: InputDecoration(hintText: 'Password'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
+
