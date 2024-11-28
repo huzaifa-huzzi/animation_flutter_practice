3b84abfc2e08a62c8ad0496fd3f009727fa45c9d
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-11-28 14:57:01 +0500

    1.Practicing the animations

---
+++ lib/ValueChangeAnimation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ValueChangeAnimation extends StatefulWidget {
+  const ValueChangeAnimation({super.key});
+
+  @override
+  State<ValueChangeAnimation> createState() => _ValueChangeAnimationState();
+}
+
+class _ValueChangeAnimationState extends State<ValueChangeAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
