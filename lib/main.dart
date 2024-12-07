109d369a7031feb3c9ab6dfd92a6a611413d48c0
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-12-07 15:12:00 +0500

    1.Practicing the animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/CardAnimations.dart';
 import 'package:untitled/CarouselAnimation.dart';
+import 'package:untitled/FlipAnimation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-    home: CarouselAnimation(),
+    home: FlipAnimation()
     );
   }
 }
