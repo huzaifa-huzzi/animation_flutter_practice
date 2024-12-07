8a44704d8b8d6758394cc88800d9ab9b089d7a71
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-12-07 15:12:26 +0500

    1.Practicing the animations

--- lib/FlipAnimation.dart
+++ lib/FlipAnimation.dart
@@ -1,6 +1,5 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-
-
 
 class FlipAnimation extends StatefulWidget {
   const FlipAnimation({super.key});
@@ -9,57 +8,74 @@ class FlipAnimation extends StatefulWidget {
   State<FlipAnimation> createState() => _FlipAnimationState();
 }
 
-class _FlipAnimationState extends State<FlipAnimation> with SingleTickerProviderStateMixin {
-
+class _FlipAnimationState extends State<FlipAnimation>
+    with SingleTickerProviderStateMixin {
   late AnimationController controller;
-  late Animation<int> flip ;
+  late Animation<double> flip;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
-    controller = AnimationController(vsync: this,duration: Duration(seconds: 5));
-    flip = Tween(begin: 0,end: -1).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.5,curve: Curves.linear)));
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
 
+    flip = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.0, 1.0, curve: Curves.linear),
+      ),
+    );
+  }
 
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(' Flip Animations'),
+        title: const Text('Flip Animation'),
       ),
       body: AnimatedBuilder(
-          animation: controller,
-          builder: (context,child){
-            return Center(
-              child: InkWell(
-                onTap: (){
+        animation: controller,
+        builder: (context, child) {
+          return Center(
+            child: InkWell(
+              onTap: () {
+                if (controller.isAnimating) {
+                  controller.stop();
+                } else {
                   controller.repeat();
-                },
+                }
+              },
+              child: Transform(
+                transform: Matrix4.identity()
+                  ..rotateX(2 * pi * flip.value),
+                alignment: Alignment.center,
                 child: Container(
                   height: 200,
                   width: 200,
-                  child: Transform(
-                      transform: Matrix4.identity() ..rotateX(2*Pi*flip.value),
-                    alignment: Alignment.center,
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.green,
-                      child: RotationTransition(
-                          turns: flip,
-                        child : Text('This flips')
-
-                      ),
+                  color: Colors.green,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'This Flips',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-            );
-          }),
+            ),
+          );
+        },
+      ),
     );
   }
 }
