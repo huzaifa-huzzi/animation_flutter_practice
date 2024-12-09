f1969ea38e81f622f75d196792b05225b7ee1b95
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-12-09 15:06:17 +0500

    1.Practicing the animations

---
+++ lib/RadialMenu.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class RadialMenu extends StatefulWidget {
+  const RadialMenu({super.key});
+
+  @override
+  State<RadialMenu> createState() => _RadialMenuState();
+}
+
+class _RadialMenuState extends State<RadialMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
