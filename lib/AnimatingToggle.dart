b35dc71cde9f7a30e10dbfad9741c52bbee06f05
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-12-10 14:59:23 +0500

    1.Practicing the animations

---
+++ lib/AnimatingToggle.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class AnimationToggle extends StatefulWidget {
+  const AnimationToggle({super.key});
+
+  @override
+  State<AnimationToggle> createState() => _AnimationToggleState();
+}
+
+class _AnimationToggleState extends State<AnimationToggle> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
